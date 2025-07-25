4e3efb1531019faf68aaa603978fae62d48889eb
tstyer <travisstyer.ts@gmail.com>
2025-07-25 11:43:22 +0100

    initiate testing in the terminal

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
+// 1: Load JS using jQuery code.
 
+$(document).ready(function () {
+
+    // jQuery to select id's
+    const $musicToggle = $('#music_toggle');
+    const $music = $('music');
+
+    // Ensure music is paused upon loading
+    $music.pause();
+
+})
