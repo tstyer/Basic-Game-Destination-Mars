2c39aa31bd682a22764ff39a222d1176ffe92f1b
tstyer <travisstyer.ts@gmail.com>
2025-07-26 11:47:37 +0100

    Complete basic jest test for audio

--- assets/tests/script.test.js
+++ assets/tests/script.test.js
@@ -12,9 +12,14 @@ const musicOn = require("../script");
 describe("Music Toggle Functionality", () => {
     describe("on switch", () => {
         test("should play music when switched on", () => {
-            expect(musicOn()).toPlay("assets\music\chill_synthwave_playlist_snapshot _white_bat_audio.mp3");
-        })
-    })
+            
+            // Mock test call as there is no audio.play matcher
+            Audio.play = jest.fn();
+
+            // First test completed
+            expect(Audio.play).toHaveBeenCalled();
+        });
+    });
 
     describe("off switch", () => {
 
@@ -23,4 +28,4 @@ describe("Music Toggle Functionality", () => {
 
 // CONTINUE WITH THE ABOVE FROM VIDEO 3 IN LETS MEET JEST. 
 
-// - You need to create a mock to test if music was called, this is because there is no macher, like .toPlay for music. Use chat GPT to help with the mocks and keep what you have as is, following the video. 
+// - You need to create a mock to test if music was called, this is because there is no matcher, like .toPlay for music. Use chat GPT to help with the mocks and keep what you have as is, following the video. 
